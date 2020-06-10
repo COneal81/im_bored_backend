5c2d21f890cba0002b97b766b772681b59a7eb8c
github username <github email address>
2020-06-10 12:00:23 -0400

    In categories_controller, added validation .valid? for create controller action.

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -7,8 +7,9 @@ class Api::V1::ActivitiesController < ApplicationController
     end
 
     def create
-        activity = Activity.new(activities_params)
-        if activity.save
+        activity = Activity.find_or_create_by(activities_params)
+        if activity.valid?
+            activity.save
             render json: ActivitiesSerializer.new(activity), status: :accepted
         else
             render json: {errors: activity.errors.full_messages}, status: :unprocessible_entity
